7464d7f8c503c7d84dba258942fb4bef84942ff0
Facundo Gauna <fgauna12@gmail.com>
2019-11-12 08:41:48 -0500

    configured cloudflare worker and working worker!

--- src/cloud-worker/index.js
+++ src/cloud-worker/index.js
@@ -1,30 +1,24 @@
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
-async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
-}
+  try {
+    return event.respondWith(handleRequest(event))
+  } catch (e) {
+    return event.respondWith(new Response('Error thrown ' + e.message))
+  }
+});
 
 async function handleRequest(event) {
   let request = event.request
-  let cacheUrl = new URL(request.url)
-  // hostname for a different zone
-  cacheUrl.hostname = someOtherHostname
+  let cacheUrl = new URL("https://azapp-sampleapi-prod.azurewebsites.net/api/values")
   let cacheKey = new Request(cacheUrl, request)
   let cache = caches.default
   // Get this request from this zone's cache
   let response = await cache.match(cacheKey)
   if (!response) {
     //if not in cache, grab it from the origin
-    response = await fetch(request)
+    response = await fetch(cacheKey)
     // must use Response constructor to inherit all of response's fields
     response = new Response(response.body, response)
+    console.log('received response', response);
     // Cache API respects Cache-Control headers, so by setting max-age to 10
     // the response will only live in cache for max of 10 seconds
     response.headers.append('Cache-Control', 'max-age=60')
@@ -34,13 +28,3 @@ async function handleRequest(event) {
   }
   return response
 }
-
-addEventListener('fetch', event => {
-  try {
-    return event.respondWith(handleRequest(event))
-  } catch (e) {
-    return event.respondWith(new Response('Error thrown ' + e.message))
-  }
-});
-
-const someOtherHostname = 'my.herokuapp.com'
