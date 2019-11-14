c6c0924baf32b5a69345817d4e630bdfaae7065f
Facundo Gauna <fgauna12@gmail.com>
2019-11-14 11:01:41 -0500

    now fixing it for real. removed some of the headers  including cache control

--- src/cloud-worker/index.js
+++ src/cloud-worker/index.js
@@ -21,13 +21,18 @@ async function handleRequest(event) {
     response = await fetch(downstreamRequest)
     // must use Response constructor to inherit all of response's fields
     response = new Response(response.body, response)
+    const newHeaders = new Headers(response.headers)
+    newHeaders.delete('Cache-Control');
+    newHeaders.delete('Server');
+    newHeaders.delete('X-Powered-By');
+    newHeaders.append('Cache-Control', 'max-age=120');
+    response.headers = newHeaders;
     // Cache API respects Cache-Control headers, so by setting max-age to 120
     // the response will only live in cache for max of 120 seconds
-    response.headers.append('Cache-Control', 'max-age=120')
+    //response.headers.append('Cache-Control', 'max-age=120')
     // store the fetched response as cacheKey
     // use waitUntil so computational expensive tasks don't delay the response
-    event.waitUntil(cache.put(response.url, response.clone()))
-    console.log('response url', backendUrl);
+    event.waitUntil(cache.put(backendUrl, response.clone()));
   }
   return response
 }
