520e0380078d2e46e52ea28b320eb69b502be0e0
Facundo Gauna <fgauna12@gmail.com>
2019-11-14 09:20:19 -0500

    attempting to fix the cache

--- src/cloud-worker/index.js
+++ src/cloud-worker/index.js
@@ -6,24 +6,28 @@ addEventListener('fetch', event => {
   }
 });
 
+const backendUrl = "https://azapp-sampleapi-prod.azurewebsites.net/api/values";
+
 async function handleRequest(event) {
   let request = event.request
-  let cacheUrl = new URL("https://azapp-sampleapi-prod.azurewebsites.net/api/values")
+  let cacheUrl = new URL(backendUrl)
   let downstreamRequest = new Request(cacheUrl, request)
   let cache = caches.default
+  console.log('backend url', backendUrl);
   // Get this request from this zone's cache
-  let response = await cache.match(request.url)
+  let response = await cache.match(backendUrl)
   if (!response) {
     //if not in cache, grab it from the origin
     response = await fetch(downstreamRequest)
     // must use Response constructor to inherit all of response's fields
     response = new Response(response.body, response)
-    // Cache API respects Cache-Control headers, so by setting max-age to 10
-    // the response will only live in cache for max of 10 seconds
+    // Cache API respects Cache-Control headers, so by setting max-age to 120
+    // the response will only live in cache for max of 120 seconds
     response.headers.append('Cache-Control', 'max-age=120')
     // store the fetched response as cacheKey
     // use waitUntil so computational expensive tasks don't delay the response
-    event.waitUntil(cache.put(request.url, response.clone()))
+    event.waitUntil(cache.put(response.url, response.clone()))
+    console.log('response url', backendUrl);
   }
   return response
 }
